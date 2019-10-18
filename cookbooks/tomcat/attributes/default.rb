0beb5e87668005ffecf5e1ac9b54329c4e73d03e
Steve Fraser <sfraser@chef.io>
2019-10-18 11:47:03 -0400

    minor cleanup

--- cookbooks/tomcat/attributes/default.rb
+++ cookbooks/tomcat/attributes/default.rb
@@ -1,5 +1,5 @@
 default['tomcat']['group'] = 'tomcat'
 default['tomcat']['home_dir'] = '/opt/tomcat'
 default['tomcat']['shell'] = '/bin/nologin'
-default['tomcat']['mode'] = '750'
+default['tomcat']['permissions'] = 0750
 default['tomcat']['user'] = 'tomcat'
--- cookbooks/tomcat/recipes/install.rb
+++ cookbooks/tomcat/recipes/install.rb
@@ -3,6 +3,7 @@ tomcat_user = node['tomcat']['user']
 tomcat_group = node['tomcat']['group']
 tomcat_dir = node['tomcat']['home_dir']
 tomcat_shell = node['tomcat']['shell']
+tomcat_permissions = node['tomcat']['permissions']
 
 # install java-7
 yum_package 'java-1.7.0-openjdk-devel' 
@@ -29,7 +30,7 @@ end
 bash 'extract tomcat archive' do
   # cwd ::File.dirname('/tmp')
   code <<-EOH
-    tar xvf /tmp/apache-tomcat-8*tar.gz -C #{node['tomcat']['home_dir']} --strip-components=1
+    tar xvf /tmp/apache-tomcat-8*tar.gz -C #{tomcat_dir} --strip-components=1
     EOH
   not_if { ::File.exist?("#{tomcat_dir}/conf") }
 end
@@ -38,7 +39,7 @@ end
 ruby_block 'set recursive ownership & permissions' do
   block do
     FileUtils.chown_R('root', tomcat_group, tomcat_dir)
-    FileUtils.chmod_R(750, "#{tomcat_dir}/conf")
+    FileUtils.chmod_R(tomcat_permissions, "#{tomcat_dir}/conf")
     FileUtils.chown_R(tomcat_user, tomcat_group, "#{tomcat_dir}/webapps")
     FileUtils.chown_R(tomcat_user, tomcat_group, "#{tomcat_dir}/work")
     FileUtils.chown_R(tomcat_user, tomcat_group, "#{tomcat_dir}/temp")
