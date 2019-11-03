d2a10f60d7b044e2f5060dbee13f2bae704ab7a5
Steve Fraser <sfraser@chef.io>
2019-11-03 08:14:01 -0500

    update

--- cookbooks/tomcat/spec/unit/recipes/default_spec.rb
+++ cookbooks/tomcat/spec/unit/recipes/default_spec.rb
@@ -29,3 +29,16 @@ describe 'tomcat::default' do
     end
   end
 end
+
+# describe 'tomcat::default' do
+#   context 'When all attributes are default, on a RHEL-based system' do
+#     let(:chef_run) do
+#       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7')
+#       runner.converge(described_recipe)
+#     end
+
+#     it 'converges successfully' do
+#       expect { chef_run }.to_not raise_error
+#     end
+#   end
+# end
