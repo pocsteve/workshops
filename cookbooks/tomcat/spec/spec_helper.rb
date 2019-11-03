00c94a969ad7480936ddba28ad22a3b942f735d1
Steve Fraser <sfraser@chef.io>
2019-11-03 07:58:41 -0500

    initial chefspec test

--- cookbooks/tomcat/spec/spec_helper.rb
+++ cookbooks/tomcat/spec/spec_helper.rb
@@ -2,3 +2,16 @@
 
 require 'chefspec'
 require 'chefspec/policyfile'
+
+describe 'tomcat::default' do
+  context 'When all attributes are default, on a RHEL-based system' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
