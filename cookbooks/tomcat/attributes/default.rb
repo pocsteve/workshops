cb7cc1aa736a589e55a816fce27fdd1516d17bf5
Steve Fraser <sfraser@chef.io>
2019-11-04 10:20:46 -0500

    cleanup

--- cookbooks/tomcat/attributes/default.rb
+++ cookbooks/tomcat/attributes/default.rb
@@ -7,11 +7,13 @@ default['tomcat']['major-version'] = '8'
 default['tomcat']['openjdk-version'] = '1.7.0'
 
 # 8.5.45
+# DON'T FORGET TO UPDATE THE INSPEC CODE!
 default['tomcat']['home_dir'] = '/opt/tomcat-8.5.45'
 default['tomcat']['version'] = '8.5.45'
 default['tomcat']['checksum'] = 'd9b58d12979243fba01bbbbb33c140c8593940c005ec9acef1b2f54ce9b3d0fc'
 
 # 8.5.47
+# DON'T FORGET TO UPDATE THE INSPEC CODE!
 # default['tomcat']['home_dir'] = '/opt/tomcat-8.5.47'
 # default['tomcat']['version'] = '8.5.47'
 # default['tomcat']['checksum'] = 'dd08cf2675675d012f87127701794f767eaa1ddfa2734c143381f39bef611761'
--- cookbooks/tomcat/spec/unit/recipes/default_spec.rb
+++ cookbooks/tomcat/spec/unit/recipes/default_spec.rb
@@ -8,17 +8,6 @@
 
 require 'spec_helper'
 
-describe 'tomcat::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
   context 'When all attributes are default, on CentOS 7' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
@@ -29,16 +18,3 @@ describe 'tomcat::default' do
     end
   end
 end
-
-# describe 'tomcat::default' do
-#   context 'When all attributes are default, on a RHEL-based system' do
-#     let(:chef_run) do
-#       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7')
-#       runner.converge(described_recipe)
-#     end
-
-#     it 'converges successfully' do
-#       expect { chef_run }.to_not raise_error
-#     end
-#   end
-# end
