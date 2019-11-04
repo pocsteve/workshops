d87bd36bc95bd44e5d18c4f1b6f2a66090ab3ab7
Steve Fraser <sfraser@chef.io>
2019-11-04 12:53:09 -0500

    fixed missing describe portion of block

--- cookbooks/tomcat/spec/unit/recipes/default_spec.rb
+++ cookbooks/tomcat/spec/unit/recipes/default_spec.rb
@@ -30,15 +30,3 @@ describe 'tomcat::default' do
   end
 end
 
-# describe 'tomcat::default' do
-#   context 'When all attributes are default, on a RHEL-based system' do
-#     let(:chef_run) do
-#       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7')
-#       runner.converge(described_recipe)
-#     end
-
-#     it 'converges successfully' do
-#       expect { chef_run }.to_not raise_error
-#     end
-#   end
-# end
