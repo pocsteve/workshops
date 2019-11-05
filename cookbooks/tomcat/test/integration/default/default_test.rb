94705e501546bca34248ed059053233964329cef
Steve Fraser <sfraser@chef.io>
2019-11-04 20:19:17 -0500

    corrected tomcat version

--- cookbooks/tomcat/test/integration/default/default_test.rb
+++ cookbooks/tomcat/test/integration/default/default_test.rb
@@ -1,7 +1,9 @@
-describe bash('ps -ef|grep tomcat') do
-  its('stdout') { should match /tomcat-8.5.47/ }
-end
+
 
 describe http('http://localhost:8080') do
   its('status') { should cmp 200 }
+end
+
+describe bash('ps -ef|grep tomcat') do
+  its('stdout') { should match /tomcat-8.5.45/ }
 end
