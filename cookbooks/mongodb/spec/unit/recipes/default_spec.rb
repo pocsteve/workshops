c7d13c28e7ad818f70016c31ecf06e866115cfb6
Steve Fraser <sfraser@chef.io>
2019-11-04 20:18:01 -0500

    added chefspec test

--- cookbooks/mongodb/spec/unit/recipes/default_spec.rb
+++ cookbooks/mongodb/spec/unit/recipes/default_spec.rb
@@ -10,12 +10,23 @@ require 'spec_helper'
 
 describe 'mongodb::default' do
   context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7').converge(described_recipe) }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs package mongodb-org' do
+      expect(chef_run).to install_yum_package('mongodb-org')
+    end
+
+    it 'creates yum repository mongodb' do
+      expect(chef_run).to create_yum_repository('mongodb').with(
+        baseurl: 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/',
+        gpgcheck: false,
+        enabled: true
+      )
+    end
+
+    it 'enables and starts mongod service' do
+      expect(chef_run).to enable_service('mongod')
+      expect(chef_run).to start_service('mongod')
     end
   end
 end
--- cookbooks/tomcat/spec/unit/recipes/default_spec.rb
+++ cookbooks/tomcat/spec/unit/recipes/default_spec.rb
@@ -10,12 +10,25 @@ require 'spec_helper'
 
 describe 'tomcat::default' do
   context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7').converge(described_recipe) }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'creates group tomcat' do
+      expect(chef_run).to create_group('tomcat')
+    end
+
+    it 'adds user tomcat to group tomcat' do
+      expect(chef_run).to create_user('tomcat').with(
+        group: 'tomcat'
+      )
+    end
+
+    it 'creates a template' do
+      expect(chef_run).to create_template('/etc/systemd/system/tomcat.service')
+    end
+
+    it 'creates a service tomcat.service' do
+      expect(chef_run).to enable_systemd_unit('tomcat.service')
+      expect(chef_run).to start_systemd_unit('tomcat.service')
     end
   end
 end
